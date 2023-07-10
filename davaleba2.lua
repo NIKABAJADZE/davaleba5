2c5ec04dc355b225c07f994c2d1e68cf796bc3e5
NIKABAJADZE <nikabajadze2006@gmail.com>
2023-07-10 23:35:12 +0400

    new

---
+++ davaleba2.lua
@@ -0,0 +1,15 @@
+math.randomseed(os.time())
+function hello(number)
+    
+
+         if number > 7 then
+    print(true) 
+         else
+             print(false)
+         end
+         
+
+end
+
+number = math.random(15)
+print(hello(number))
